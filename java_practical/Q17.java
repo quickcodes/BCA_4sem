76f5ec08f87b62066f71e1a2d7f37955a649605f
Harsh_f(x) <69109482+hashfx@users.noreply.github.com>
2022-06-03 16:12:45 +0530

    Create Q17.java

---
+++ java_practical/Q17.java
@@ -0,0 +1,29 @@
+// Base Class
+class Parent {
+	void show()
+	{
+		System.out.println("Parent's show()");
+	}
+}
+
+// Inherited class
+class Child extends Parent {
+
+	@Override
+	void show()
+	{
+		System.out.println("Child's show()");
+	}
+}
+
+// Driver class
+class Q17 {
+	public static void main(String[] args)
+	{
+		Parent obj1 = new Parent();
+		obj1.show();
+
+		Parent obj2 = new Child();
+		obj2.show();
+	}
+}
